38d5b0f2e41db969efc839dd6e12ef6c81aedba8
jaymwin <jmwiniarski@gmail.com>
2025-02-12 10:37:29 -0600

    add function to read ARU deployments

---
+++ R/read_aru_deployments.R
@@ -0,0 +1,15 @@
+
+#' Read ARU deployment data from Google Sheets
+#'
+#' @param google_sheet_url
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+read_aru_deployments <- function(google_sheet_url) {
+
+  read_sheet(google_sheet_url)
+
+}
