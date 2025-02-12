c40ee61c188fb6cba1f061aa7303dcbd3daaab45
jaymwin <jmwiniarski@gmail.com>
2025-02-12 11:21:37 -0600

    need to modify for pond file structure

---
+++ R/utils.R
@@ -0,0 +1,49 @@
+
+# temporary folders to hold wavs on desktop or other local folder
+create_temp_folders <- function(wav_file, input_dir, temp_dir) {
+
+  stringr::str_replace(
+    stringr::str_remove(wav_file, basename(wav_file)),
+    input_dir,
+    stringr::str_c(temp_dir, '/')
+  ) |>
+    fs::dir_create()
+
+}
+
+# create flac folders on an external hard drive
+create_flac_folders <- function(wav_file, output_dir, pond_id, visit_id, swift_id) {
+
+  date <- stringr::str_extract(wav_file, '[0-9]{8}')
+  fs::dir_create(path = stringr::str_glue('{output_dir}/{pond_id}/{pond_id}_{visit_id}/{pond_id}_{visit_id}_{date}'))
+
+}
+
+
+# function to handle the file conversion process
+convert_to_flac <- function(wav_file, input_dir, temp_dir, output_dir, pond_id, visit_id, swift_id) {
+
+  temp_path <- stringr::str_c(stringr::str_replace(stringr::str_remove(wav_file, basename(wav_file)), input_dir, temp_dir), basename(wav_file))
+
+  fs::file_copy(
+    path = wav_file,
+    new_path = temp_path,
+    overwrite = FALSE
+  )
+
+  # Define the output flac file path
+  output_file <- file.path(stringr::str_glue('{output_dir}/{site_name}/{cell_id}/{station_id}/{swift_id}_{stringr::str_extract(wav_file, "[0-9]{8}")}/{stringr::str_replace(basename(wav_file), ".wav", ".flac")}'))
+
+  # Use sox to convert the wav file to flac
+  # The '-C 8' option sets compression level for FLAC (range 0-8, where 8 is the highest compression)
+  seewave::sox(
+    stringr::str_glue("{temp_path} -C 5 {output_file}"),
+    path2exe = "C:/Program Files (x86)/sox-14-4-2"
+  )
+
+  # message('file compressed')
+
+  # then clear
+  fs::file_delete(temp_path)
+
+}
